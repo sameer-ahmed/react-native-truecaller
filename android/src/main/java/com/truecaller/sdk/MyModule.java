66ad0aac17e30d8bdbf99aebac4fc9b2c769be6d
sameer-ahmed <me@sameerahmed.in>
2018-12-27 17:54:58 +0530

    version 1.1.2 fixed isTrueCallerInstalled not working for android

--- android/src/main/java/com/truecaller/sdk/MyModule.java
+++ android/src/main/java/com/truecaller/sdk/MyModule.java
@@ -5,13 +5,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
-import android.widget.Toast;
-
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -23,17 +21,16 @@ import com.truecaller.android.sdk.TrueClient;
 import com.truecaller.android.sdk.TrueError;
 import com.truecaller.android.sdk.TrueProfile;
 
-
-public class MyModule extends ReactContextBaseJavaModule implements ITrueCallback{
+public class MyModule extends ReactContextBaseJavaModule implements ITrueCallback {
 
     private Callback mErrorCall;
     private Callback mSuccessCall;
     private TrueClient mTrueClient;
-    private String TAG="MyModule";
+    private String TAG = "MyModule";
     private String mTruecallerRequestNonce;
 
-    private  ReactContext reactContext;
-    private ActivityEventListener mEventListener =new BaseActivityEventListener(){
+    private ReactContext reactContext;
+    private ActivityEventListener mEventListener = new BaseActivityEventListener() {
         @Override
         public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
             if (null != mTrueClient && mTrueClient.onActivityResult(requestCode, resultCode, data)) {
@@ -42,10 +39,9 @@ public class MyModule extends ReactContextBaseJavaModule implements ITrueCallbac
         }
     };
 
-
     public MyModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        this.reactContext=reactContext;
+        this.reactContext = reactContext;
         reactContext.addActivityEventListener(mEventListener);
     }
 
@@ -54,12 +50,11 @@ public class MyModule extends ReactContextBaseJavaModule implements ITrueCallbac
         return "MyModule";
     }
 
-    
     @ReactMethod
-    public boolean isTruecallerInstalled() {
-        return isAppInstalled("com.truecaller");
+    public void isTruecallerInstalled(Promise promise) {
+        final Boolean isInstalled = isAppInstalled("com.truecaller");
+        promise.resolve(isInstalled);
     }
-
 
     @ReactMethod
     public void initializeClient() {
@@ -69,107 +64,99 @@ public class MyModule extends ReactContextBaseJavaModule implements ITrueCallbac
         mTruecallerRequestNonce = mTrueClient.generateRequestNonce();
     }
 
-
     @ReactMethod
     public void click(
-            String msg,
-            Callback errorCallback,
-            Callback successCallback) {
+        String msg,
+        Callback errorCallback,
+        Callback successCallback) {
         try {
-            this.mErrorCall=errorCallback;
-            this.mSuccessCall=successCallback;
+            this.mErrorCall = errorCallback;
+            this.mSuccessCall = successCallback;
             getUserDetail();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-    void getUserDetail(){
+    void getUserDetail() {
         Activity activity = getCurrentActivity();
         mTrueClient.getTruecallerUserProfile(activity);
     }
 
-
     @ReactMethod
-    public void requestTrueProfile()
-     {
-         Activity activity = getCurrentActivity();
-         mTrueClient.getTruecallerUserProfile(activity);
-     }
+    public void requestTrueProfile() {
+        Activity activity = getCurrentActivity();
+        mTrueClient.getTruecallerUserProfile(activity);
+    }
+
     @Override
-    public void onSuccesProfileShared(@NonNull TrueProfile trueProfile)
-    {
+    public void onSuccesProfileShared(@NonNull TrueProfile trueProfile) {
 
         WritableMap params = Arguments.createMap();
 
+        params.putString("firstName", trueProfile.firstName);
 
-        params.putString("firstName",trueProfile.firstName);
+        params.putString("lastName", trueProfile.lastName);
 
-        params.putString("lastName",trueProfile.lastName);
+        params.putString("phoneNumber", trueProfile.phoneNumber);
 
-        params.putString("phoneNumber",trueProfile.phoneNumber);
+        params.putString("gender", trueProfile.gender);
 
-        params.putString("gender",trueProfile.gender);
+        params.putString("street", trueProfile.street);
 
-        params.putString("street",trueProfile.street);
+        params.putString("city", trueProfile.city);
 
-        params.putString("city",trueProfile.city);
+        params.putString("zipcode", trueProfile.zipcode);
 
-        params.putString("zipcode",trueProfile.zipcode);
+        params.putString("countryCode", trueProfile.countryCode);
 
-        params.putString("countryCode",trueProfile.countryCode);
+        params.putString("facebookId", trueProfile.facebookId);
 
-        params.putString("facebookId",trueProfile.facebookId);
+        params.putString("twitterId", trueProfile.twitterId);
 
-        params.putString("twitterId",trueProfile.twitterId);
+        params.putString("email", trueProfile.email);
 
-        params.putString("email",trueProfile.email);
+        params.putString("url", trueProfile.url);
 
-        params.putString("url",trueProfile.url);
+        params.putString("avatarUrl", trueProfile.avatarUrl);
 
-        params.putString("avatarUrl",trueProfile.avatarUrl);
+        params.putBoolean("isTrueName", trueProfile.isTrueName);
 
-        params.putBoolean("isTrueName",trueProfile.isTrueName);
+        params.putBoolean("isAmbassador", trueProfile.isAmbassador);
 
-        params.putBoolean("isAmbassador",trueProfile.isAmbassador);
+        params.putString("companyName", trueProfile.companyName);
 
-        params.putString("companyName",trueProfile.companyName);
+        params.putString("jobTitle", trueProfile.jobTitle);
 
-        params.putString("jobTitle",trueProfile.jobTitle);
+        params.putString("payload", trueProfile.payload);
 
-        params.putString("payload",trueProfile.payload);
+        params.putString("signature", trueProfile.signature);
 
-        params.putString("signature",trueProfile.signature);
+        params.putString("signatureAlgorithm", trueProfile.signatureAlgorithm);
 
-        params.putString("signatureAlgorithm",trueProfile.signatureAlgorithm);
+        params.putString("requestNonce", trueProfile.requestNonce);
 
-        params.putString("requestNonce",trueProfile.requestNonce);
+        params.putBoolean("isSimChanged", trueProfile.isSimChanged);
 
-        params.putBoolean("isSimChanged",trueProfile.isSimChanged);
-
-        params.putString("verificationMode",trueProfile.verificationMode);
+        params.putString("verificationMode", trueProfile.verificationMode);
 
         sendEvent(reactContext, "didReceiveTrueProfileResponse", params);
-
     }
 
     @Override
     public void onFailureProfileShared(@NonNull TrueError trueError) {
         WritableMap params = Arguments.createMap();
-        params.putString("profile","error");
+        params.putString("profile", "error");
 
         sendEvent(reactContext, "didReceiveTrueProfileResponseError", params);
-
     }
 
-
     private void sendEvent(ReactContext reactContext,
-                           String eventName,
-                           @Nullable WritableMap params) {
+        String eventName,
+        @Nullable WritableMap params) {
         reactContext
-                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit(eventName, params);
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+            .emit(eventName, params);
     }
 
     private boolean isAppInstalled(String uri) {
@@ -181,6 +168,6 @@ public class MyModule extends ReactContextBaseJavaModule implements ITrueCallbac
         } catch (PackageManager.NameNotFoundException e) {
             installed = false;
         }
-            return installed;
+        return installed;
     }
 }
--- android/src/main/java/com/truecaller/sdk/Utils.java
+++ android/src/main/java/com/truecaller/sdk/Utils.java
@@ -11,10 +11,10 @@ public class Utils {
     static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     static SecureRandom rnd = new SecureRandom();
 
-    public static String randomString( int len ){
-        StringBuilder sb = new StringBuilder( len );
-        for( int i = 0; i < len; i++ )
-            sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
+    public static String randomString(int len) {
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++)
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
         return sb.toString();
     }
 }
